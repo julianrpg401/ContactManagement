d182bd02dc8886e9425f0dda8a5549c85ec71cf2
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-18 14:52:21 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+namespace ContactManagement
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapStaticAssets();
+
+            app.MapControllerRoute
+            (
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}"
+            ).WithStaticAssets();
+
+            app.Run();
+        }
+    }
+}
