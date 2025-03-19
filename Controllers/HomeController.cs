d07189302ea2da27c208da29e17765836b9cbc04
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-19 14:44:56 -0500

    Nothing

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,7 +6,7 @@ namespace ContactManagement.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger<HomeController> _logger; 
 
         public HomeController(ILogger<HomeController> logger)
         {
--- DataAccess/ContactRepository.cs
+++ DataAccess/ContactRepository.cs
@@ -1,38 +1,130 @@
 ﻿using ContactManagement.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace ContactManagement.DataAccess
 {
     public class ContactRepository : IContactRepository
     {
-        public Task<IActionResult> AddContactAsync(Contact contact)
+        private readonly AppDbContext _context;
+
+        public ContactRepository(AppDbContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<IActionResult> DeleteContactAsync(int id)
+        public async Task<IActionResult> AddContactAsync(Contact contact)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _context.Contacts.Add(contact);
+                await _context.SaveChangesAsync();
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
         }
 
-        public Task<IActionResult> DeleteContactsAsync()
+        public async Task<IActionResult> DeleteContactAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contact = await _context.Contacts.FindAsync(id);
+                if (contact == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                _context.Contacts.Remove(contact);
+                await _context.SaveChangesAsync();
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
         }
 
-        public Task<IActionResult> GetContactAsync(int id)
+        public async Task<IActionResult> DeleteContactsAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contacts = _context.Contacts.ToList();
+
+                _context.Contacts.RemoveRange(contacts);
+                await _context.SaveChangesAsync();
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
         }
 
-        public Task<IActionResult> GetContactsAsync()
+        public async Task<IActionResult> GetContactAsync(int id)
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contact = await _context.Contacts.FindAsync(id);
+
+                if (contact == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                return new OkObjectResult(contact);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
         }
 
-        public Task<IActionResult> UpdateContactAsync(int id, Contact contact)
+        public async Task<IActionResult> GetContactsAsync()
         {
-            throw new NotImplementedException();
+            try
+            {
+                var contacts = await _context.Contacts.ToListAsync();
+
+                return new OkObjectResult(contacts);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
+        }
+
+        public async Task<IActionResult> UpdateContactAsync(int id, Contact contact)
+        {
+            try
+            {
+                var existingContact = await _context.Contacts.FindAsync(id);
+
+                if (existingContact == null)
+                {
+                    return new NotFoundResult();
+                }
+
+                existingContact.FirstName = contact.FirstName;
+                existingContact.LastName = contact.LastName;
+                existingContact.Email = contact.Email;
+                existingContact.Phone = contact.Phone;
+                existingContact.Address = contact.Address;
+                existingContact.City = contact.City;
+
+                _context.Contacts.Update(existingContact);
+                await _context.SaveChangesAsync();
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
         }
     }
 }
