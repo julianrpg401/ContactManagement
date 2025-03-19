eaae7e8e606849b4f99af03af8d2fff9e7c72f73
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-19 13:00:01 -0500

    Add repository pattern

---
+++ DataAccess/ContactRepository.cs
@@ -0,0 +1,38 @@
+﻿using ContactManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagement.DataAccess
+{
+    public class ContactRepository : IContactRepository
+    {
+        public Task<IActionResult> AddContactAsync(Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteContactAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteContactsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetContactAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> GetContactsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateContactAsync(int id, Contact contact)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DataAccess/IContactRepository.cs
@@ -0,0 +1,15 @@
+﻿using ContactManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagement.DataAccess
+{
+    public interface IContactRepository
+    {
+        Task<IActionResult> AddContactAsync(Contact contact);
+        Task<IActionResult> GetContactAsync(int id);
+        Task<IActionResult> GetContactsAsync();
+        Task<IActionResult> DeleteContactAsync(int id);
+        Task<IActionResult> UpdateContactAsync(int id, Contact contact);
+        Task<IActionResult> DeleteContactsAsync();
+    }
+}
