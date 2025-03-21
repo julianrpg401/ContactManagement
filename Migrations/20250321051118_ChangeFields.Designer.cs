8556f618c673285d359a753cabdc6f8a1d4867c7
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-21 02:00:44 -0500

    Add migrations

--- Migrations/20250321051118_ChangeFields.Designer.cs
+++ Migrations/20250321051118_ChangeFields.Designer.cs
@@ -3,6 +3,7 @@ using ContactManagement.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ContactManagement.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250321051118_ChangeFields")]
+    partial class ChangeFields
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
