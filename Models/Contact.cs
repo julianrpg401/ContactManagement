921ec5266b9b546e4062cb2c3488525be8cbe04c
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-03-21 02:01:14 -0500

    Update model

--- Models/Contact.cs
+++ Models/Contact.cs
@@ -1,6 +1,4 @@
-﻿
-
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace ContactManagement.Models
 {
@@ -8,12 +6,16 @@ namespace ContactManagement.Models
     {
         [Key]
         public int ContactId { get; set; }
-        public string? FirstName { get; set; } = string.Empty;
-        public string LastName { get; set; } = string.Empty;
-        public string? Email { get; set; }
+        [MaxLength(50)]
+        public string FirstName { get; set; } = string.Empty;
+        [MaxLength(50)]
+        public string? LastName { get; set; } = string.Empty;
         [MaxLength(100)]
+        public string? Email { get; set; }
+        [MaxLength(10)]
         public string Phone { get; set; } = string.Empty;
         public string? Address { get; set; }
+        [MaxLength(50)]
         public string? City { get; set; }
     }
 }
