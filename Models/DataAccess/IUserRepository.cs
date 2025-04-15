2ec60721aabe4614f166c7706630e503d092950f
Julian Garcia <104118907+julianrpg401@users.noreply.github.com>
2025-04-14 22:58:48 -0500

    Adding repositories for data access

---
+++ Models/DataAccess/IUserRepository.cs
@@ -0,0 +1,13 @@
+﻿using ContactManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManagement.DataAccess
+{
+    public interface IUserRepository
+    {
+        Task<IActionResult> AddUserAsync(User user);
+        Task<IActionResult> UpdateUserAsync(int id, User user);
+        Task<IActionResult> DeleteUserAsync(int id);
+        Task<User?> ValidateUserAsync(User user);
+    }
+}
---
+++ Models/DataAccess/UserRepository.cs
@@ -0,0 +1,61 @@
+﻿using ContactManagement.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContactManagement.DataAccess
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> AddUserAsync(User user)
+        {
+            try
+            {
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return new OkResult();
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(new { Message = ex.Message });
+            }
+        }
+
+        public Task<IActionResult> DeleteUserAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> DeleteUsersAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IActionResult> UpdateUserAsync(int id, User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<User?> ValidateUserAsync(User user)
+        {
+            try
+            {
+                var existingUser = await _context.Users
+                    .FirstOrDefaultAsync(u => u.Email == user.Email);
+
+                return existingUser;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
